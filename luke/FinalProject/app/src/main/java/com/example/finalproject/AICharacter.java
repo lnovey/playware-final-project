60c2b86bfc22a24c8988c798ce2f495363e8a4a5
Luke Novey <Luke@lukes-mbp.eduroam.wireless.dtu.dk>
2019-04-30 20:24:20 +0200

    got game loop working in a janky way with going to new activity for fights

--- luke/FinalProject/app/src/main/java/com/example/finalproject/AICharacter.java
+++ luke/FinalProject/app/src/main/java/com/example/finalproject/AICharacter.java
@@ -1,20 +1,105 @@
 package com.example.finalproject;
 
-public class AICharacter extends Character {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class AICharacter extends Character implements Parcelable {
+
+    public int intelligence, numHeals, maxHealth;
+
     public AICharacter(int health, int level, int attack, int defense, int experience) {
         super(health, level, attack, defense, experience);
     }
 
+
     @Override
     BattleAction battleTurn() {
-        BattleAction returner = new BattleAction();
-        if (this.health <= 30){
-            returner.type = 3;
-        } else {
-            returner.type = 1;
+        BattleAction returner;// = new BattleAction();
+        switch (intelligence) {
+            case 0:
+                returner = dumbTurn();
+                break;
+            case 1:
+                returner = mediumTurn();
+                break;
+            case 2:
+                returner = hardTurn();
+                break;
+            default:
+                returner = dumbTurn();
+                break;
+
         }
+
+
         return returner;
 
     }
 
+    BattleAction hardTurn() {
+        BattleAction returner = new BattleAction();
+
+        return returner;
+    }
+
+    BattleAction mediumTurn() {
+        BattleAction returner = new BattleAction();
+        if (this.health <= 5) {
+            returner.type = 3;
+            returner.info[0] = 10;
+        } else {
+            returner.type = 1;
+        }
+        return returner;
+    }
+
+    BattleAction dumbTurn() {
+        BattleAction returner = new BattleAction();
+        returner.type = 1;
+        return returner;
+    }
+
+
+    //PARCEL THINGS
+
+    public AICharacter(Parcel in) {
+        super(in.readInt(), in.readInt(), in.readInt(), in.readInt(), in.readInt());
+        intelligence = in.readInt();
+        numHeals = in.readInt();
+        maxHealth = in.readInt();
+        weaponAttack = in.readInt();
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(health);
+        dest.writeInt(level);
+        dest.writeInt(attack);
+        dest.writeInt(defense);
+        dest.writeInt(experience);
+        dest.writeInt(intelligence);
+        dest.writeInt(numHeals);
+        dest.writeInt(maxHealth);
+        dest.writeInt(weaponAttack);
+
+
+    }
+
+    public static final Parcelable.Creator<AICharacter> CREATOR
+            = new Parcelable.Creator<AICharacter>() {
+        public AICharacter createFromParcel(Parcel in) {
+            return new AICharacter(in);
+        }
+
+        @Override
+        public AICharacter[] newArray(int size) {
+            return new AICharacter[size];
+        }
+    };
 }
--- luke/FinalProject/app/src/main/java/com/example/finalproject/HumanCharacter.java
+++ luke/FinalProject/app/src/main/java/com/example/finalproject/HumanCharacter.java
@@ -1,5 +1,7 @@
 package com.example.finalproject;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -7,37 +9,59 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class HumanCharacter extends Character {
+public class HumanCharacter extends Character implements Parcelable{
     boolean isUserTurn;
-    ArrayList<String> battleChoices = new ArrayList<String>();
-    ListView listView;
-    ArrayAdapter<String> adapter;
-    public LinearLayout layout;
-    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-//ll.addView(myButton, lp);
+    int maxHealth;
+
 
     public HumanCharacter(int health, int level, int attack, int defense, int experience){
         super(health, level, attack, defense, experience);
+        //this.maxHealth = health;
     }
 
     @Override
     BattleAction battleTurn() {
-        isUserTurn = true;
-        layout.addView(listView, lp);
-
-
-        battleChoices.add("attack!");
-        battleChoices.add("item!");
-//
-//        adapter = new ArrayAdapter<String>(layout,android.R.layout.simple_list_item_1, battleChoices);
-//        listView.setAdapter(adapter);
-//        listView.setOnItemClickListener(messageClickedHandler);
-
-        while (isUserTurn){
-
-
-        }
-
         return null;
     }
+
+
+    //PARCEL THINGS
+
+    public HumanCharacter(Parcel in) {
+        super(in.readInt(), in.readInt(), in.readInt(), in.readInt(), in.readInt());
+        maxHealth = in.readInt();
+        weaponAttack = in.readInt();
+
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(health);
+        dest.writeInt(level);
+        dest.writeInt(attack);
+        dest.writeInt(defense);
+        dest.writeInt(experience);
+        dest.writeInt(maxHealth);
+        dest.writeInt(weaponAttack);
+
+
+    }
+
+    public static final Parcelable.Creator<HumanCharacter> CREATOR
+            = new Parcelable.Creator<HumanCharacter>() {
+        public HumanCharacter createFromParcel(Parcel in) {
+            return new HumanCharacter(in);
+        }
+
+        @Override
+        public HumanCharacter[] newArray(int size) {
+            return new HumanCharacter[size];
+        }
+    };
+
 }
